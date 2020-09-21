51ff12bb3452d6c72917b1b9895ee15791de0152
Kranberri <56488228+kran27@users.noreply.github.com>
2020-09-20 18:06:03 -0700

    Add files via upload

---
+++ Fallout Terminal Launcher.bat
@@ -0,0 +1,52 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFDBdSRaDMleeCbYJ5e31+/m7hEwRWO0qbM/326GAI+gW+HnCdJoh2W9O2P8JHhZRcBOnLiM1vGBBvmGXecKEtm8=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+title Fallout Terminal Launcher
+color 0a
+cls
+mode 41,14
+echo Welcome to ROBCO Industries (TM) Termlink
+echo Selection Required
+echo.
+cmdmenusel 0aa0 "Fallout" "Fallout 2" "Fallout Tactics" "Fallout 3" "Fallout 3 GOTY" "Fallout: New Vegas" "Fallout Shelter" "Fallout 4" "Fallout 4 VR" "Fallout 76"
+if %ERRORLEVEL%==1 start steam://rungameid/38400
+if %ERRORLEVEL%==2 start steam://rungameid/38410
+if %ERRORLEVEL%==3 start steam://rungameid/38420
+if %ERRORLEVEL%==4 start steam://rungameid/22300
+if %ERRORLEVEL%==5 start steam://rungameid/22370
+if %ERRORLEVEL%==6 start steam://rungameid/22380
+if %ERRORLEVEL%==7 start steam://rungameid/588430
+if %ERRORLEVEL%==8 start steam://rungameid/377160
+if %ERRORLEVEL%==9 start steam://rungameid/611660
+if %ERRORLEVEL%==10 start steam://rungameid/1151340
